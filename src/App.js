06d6ca0ddd2abc393035f9d3e0a353e3a74fa6b5
Jeffrey Fan <jeffreyfan93@gmail.com>
2019-11-13 21:22:17 -0800

    created simple nav bar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,122 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+  const navigation = require('./json/navigation.json');
+  const [currCity, updateCurrCity] = useState(navigation.cities[0].section);
+  const [currTime, updateCurrTime] = useState();
+  const timeZone = {
+    'cupertino': -8,
+    'new-york-city': -5,
+    'london': 0,
+    'amsterdam': +1,
+    'tokyo': +9,
+    'hong-kong': +8,
+    'sydney': +11
+  };
+
+  useEffect(() => {
+    const currDiv = document.getElementById(currCity);
+    const sliderUnderline = document.getElementById('slider-underline');
+    sliderUnderline.style.left = `${currDiv.offsetLeft}px`;
+    sliderUnderline.style.width = `${currDiv.offsetWidth}px`;
+  }, [currCity]);
+  
+  useEffect(() => {
+    getTime();
+    const startTimer = setInterval(() => {
+      getTime();
+    }, 1000)
+    return () => {clearInterval(startTimer)}
+  }, [currCity]);
+  
+
+  const getTime = () => {
+    const time = new Date();
+    const hour = time.getHours();
+    const minute = time.getMinutes();
+    const second = time.getSeconds();
+    const adjustedHour = getCorrectTimeZone(hour);
+    const formattedTime = formatTime(adjustedHour, minute, second);
+
+    updateCurrTime(formattedTime);
+  };
+
+  const getCorrectTimeZone = (hour) => {
+    switch (currCity) {
+      case 'cupertino':
+        return hour;
+      case 'new-york-city':
+      case 'london':
+      case 'amsterdam':
+      case 'tokyo':
+      case 'hong-kong':
+      case 'sydney':
+          return (hour - timeZone['cupertino'] + timeZone[currCity]) % 24;
+      default:
+        return null;
+    }
+  };
+
+  const formatTime = (hour, minute, second) => {
+    let suffix = 'AM';
+    if (hour > 11) {
+      suffix = 'PM';
+    }
+    if (hour > 12) {
+      hour -= 12;
+    } else if (hour === 0) {
+      hour = 12;
+    }
+    if (minute < 10) {
+      minute = `0${minute}`;
+    }
+    if (second < 10) {
+      second = `0${second}`;
+    }
+
+    return `${hour}:${minute}:${second} ${suffix}`
+  };
+
+  const renderCities = (cities) => {
+    let citiesList = [];
+
+    cities.forEach((city, cityKey) => {
+      citiesList.push(
+        <button
+          id={city.section}
+          className={currCity === city.section ? 'current-city' : null}
+          onClick={handleClick}
+          key={cityKey}
         >
-          Learn React
-        </a>
-      </header>
+          {city.label}
+        </button>
+      )
+    });
+
+    return citiesList;
+  };
+
+  const handleClick = (e) => {
+    const city = e.currentTarget.id;
+    updateCurrCity(city);
+  };
+
+  return (
+    <div>
+      <div className='cities-list'>
+        {
+          navigation &&
+          navigation.cities &&
+          renderCities(navigation.cities)
+        }
+      </div>
+      <div className='slider'>
+        <div id='slider-underline'/>
+      </div>
+      <div className='time-display'>
+        {currTime}
+      </div>
     </div>
   );
 }
